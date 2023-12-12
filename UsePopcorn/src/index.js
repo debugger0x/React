46060c091b8fbe1a6e20b3326d589da06e7cccf2
Johnpaul <indisputable.jay@gmail.com>
2023-12-12 20:04:04 +0100

    redoing for better understanding

--- UsePopcorn/src/index.js
+++ UsePopcorn/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
+import StarRatings from "./StarRatings";
 // import "./index.css";
 // import App from "./App";
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    {/* <StarRating maxRating={5} /> */}
+    <StarRatings maxRating={5} />
   </React.StrictMode>
 );
