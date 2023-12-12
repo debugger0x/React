9cbc1f5a7be57f1084e783fadb9ec180389950f3
Johnpaul <indisputable.jay@gmail.com>
2023-12-12 20:28:19 +0100

    cleared my own version so i can smothly follow with the tutorial

--- UsePopcorn/src/index.js
+++ UsePopcorn/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import StarRating from "./StarRating";
-import StarRatings from "./StarRatings";
+import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
@@ -9,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <StarRating maxRating={5} /> */}
-    <StarRatings maxRating={5} />
+    {/* <StarRatings maxRating={6} /> */}
+    <StarRating maxRating={6} />
   </React.StrictMode>
 );
